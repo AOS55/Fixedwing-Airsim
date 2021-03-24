8d276f928245d8923ae901f23673d32a9ebe4368
Alexander Quessy <aq15777@bristol.ac.uk>
2021-03-24 12:15:42 +0000

    test dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
-FROM ubuntu:18.04
+FROM python:3.8.8
 
 WORKDIR /app
 
-COPY src/segmentation/deeplabv3_example.py ./
+COPY requirements.txt ./
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+RUN ./python-test.sh
+
+# ENV PORT=8080
+
+# EXPOSE 8080
+
+# CMD 
--- src/HPC_runs/python-test.sh
+++ src/HPC_runs/python-test.sh
@@ -1,12 +1,2 @@
-#!/bin/bash
-
-#SBATCH --job-name=test_job
-#SBATCH --partition=test
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=1
-#SBATCH --time=0:0:10
-#SBATCH --mem=100M
-
-
-echo 'This job has run'
+echo 'Starting python'
+python3 test-python.py
