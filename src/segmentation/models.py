9c83878affd2f8044051f4f01066580b68c242ac
Alexander Quessy <aq15777@bristol.ac.uk>
2021-04-03 16:25:17 +0100

    added a num_classes parameter to the get_model function

--- src/segmentation/models.py
+++ src/segmentation/models.py
@@ -3,21 +3,22 @@ import segmentation_models_pytorch as smp
 import torchvision.models as models
 
 
-def get_model(model_name: str, device: str) -> torch.nn:
+def get_model(model_name: str, device, num_classes: int) -> torch.nn:
     """
     Gets the model requested from the config file
 
     :param model_name: name of model used in the program
     :param device: name of device model is hosted on
+    :param num_classes: number of clases in the model (including background 0,0,0)
     :return: the nn.model to be trained
     """
     model_dict = {'deeplabv3': torch.hub.load('pytorch/vision:v0.9.0', 'deeplabv3_resnet101',
-                                              pretrained=False, num_classes=3).to(device).eval(),
+                                              pretrained=False, num_classes=num_classes).to(device).eval(),
                   'UnetPlusPlus': smp.UnetPlusPlus(encoder_name='resnet34', encoder_depth=5,
-                                                   encoder_weights=None, classes=3).to(device).eval(),
+                                                   encoder_weights=None, classes=num_classes).to(device).eval(),
                   'deeplabv3plus': smp.DeepLabV3Plus(encoder_name='resnet101', encoder_depth=5,
-                                                     encoder_weights=None, classes=3).to(device).eval(),
-                  'resnet18': models.segmentation.fcn_resnet50(pretrained=False, num_classes=3)}
+                                                     encoder_weights=None, classes=num_classes).to(device).eval(),
+                  'resnet18': models.segmentation.fcn_resnet50(pretrained=False, num_classes=num_classes)}
     try:
         model = model_dict[model_name]
     except KeyError:
