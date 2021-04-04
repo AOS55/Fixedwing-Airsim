40913a1457eeeb4b3b42af4b77bac4aff03ce315
Alexander Quessy <aq15777@bristol.ac.uk>
2021-04-04 10:48:02 +0100

    added and tested a CityscapesDataset class

--- src/segmentation/dataset_manager.py
+++ src/segmentation/dataset_manager.py
@@ -4,45 +4,8 @@ import torch
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader, Subset
-from torchvision import transforms
+from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
-
-# class UAVDataset(RunwaysDataset):
-#     """
-#     Prepare input and training data from the iUAV dataset https://uavid.nl/
-#     """
-#
-#     def __init__(self,
-#                  root_dir: str,
-#                  labels: dict,
-#                  semantic_dir: str = "segmentation_masks",
-#                  image_dir: str = "images"):
-#         """
-#         Args:
-#         :param root_dir: Root directory of all dataset files
-#         :param semantic_dir: path to directory containing all semantic images
-#         :param image_dir: path containing all raw images
-#         :param labels: dictionary containing the rgb triplets and labels of the image mask
-#         """
-#         self.root_dir = root_dir
-#         self.semantic_dir = semantic_dir
-#         self.image_dir = image_dir
-#         self.labels = labels
-#
-#     def __len__(self):
-#         """
-#         Get the number of frames in the dataset
-#         :return: simply the number of files in the semantic image directory
-#         """
-#         dirname = os.path.join(self.root_dir, self.semantic_dir)
-#         return len([name for name in os.listdir(dirname) if os.path.isfile(os.path.join(dirname, name))])
-#
-#     def __getitem__(self, idx):
-#         """
-#         Get samples from the dataset
-#         :param idx: the index of the tensor requested
-#         :return: 2 dicts of tensors image and mask for test and train samples
-#         """
 
 
 class RunwaysDataset(Dataset):
@@ -140,7 +103,7 @@ class UAVDataset(RunwaysDataset):
     |   |_segmentation_masks
     """
     def __init__(self, root_dir: str, labels: dict, crop_size: tuple = (480, 852)):
-        super().__init__(root_dir, labels, )
+        super().__init__(root_dir, labels)
         self.root_dir = root_dir
         self.labels = labels
         self.crop_size = crop_size
@@ -150,27 +113,53 @@ class UAVDataset(RunwaysDataset):
         image_loc = os.path.join(self.root_dir, self.image_dir, str(idx) + ".png")
         input_mask = Image.open(mask_loc)
         input_image = Image.open(image_loc)
-        input_image, input_mask = self.apply_random_crop(input_image, input_mask)
+        input_image, input_mask = apply_random_crop(input_image, input_mask, self.crop_size)
         mask_tensor = self.mask_preparation(input_mask)
         image_tensor = self.image_preparation(input_image)
         sample = {'image': image_tensor, 'mask': mask_tensor}
         return sample
 
-    def apply_random_crop(self, img: Image.Image, tgt: Image.Image,):
-        """
-        Apply a random crop equally to the target and the image
 
-        :param img: a PIL Image of the RGB image
-        :param tgt: a PIL Image of the target image
-        :param scale: an HxW scale of the desired crop e.g. 480x852
-        :return: the cropped img & tgt tensors
-        """
-        t = transforms.RandomResizedCrop(self.crop_size)
-        state = torch.get_rng_state()
-        img = t(img)
-        torch.set_rng_state(state)
-        tgt = t(tgt)
-        return img, tgt
+class CityscapesDataset(RunwaysDataset):
+    """
+    Loads in the cityscapes dataset using the dataloader technique
+    """
+    def __init__(self, root_dir: str, labels: dict, crop_size: tuple = (480, 852), split_type: str = 'test'):
+        super().__init__(root_dir, labels)
+        self.root_dir = root_dir
+        self.labels = labels
+        self.crop_size = crop_size
+        self.cityscape_set = datasets.Cityscapes(root_dir, split=split_type, mode='fine',
+                                                 target_type='semantic')
+
+    def __len__(self):
+        return len(self.cityscape_set)
+
+    def __getitem__(self, idx):
+        input_image = self.cityscape_set[idx][0]
+        input_mask = self.cityscape_set[idx][1]
+        input_image, input_mask = apply_random_crop(input_image, input_mask, self.crop_size)
+        mask_tensor = torch.tensor(np.array(input_mask)).to(dtype=int)
+        image_tensor = self.image_preparation(input_image)
+        sample = {'image': image_tensor, 'mask': mask_tensor}
+        return sample
+
+
+def apply_random_crop(img: Image.Image, tgt: Image.Image, crop_size: tuple = (480, 852)):
+    """
+    Apply a random crop equally to the target and the image
+
+    :param img: a PIL Image of the RGB image
+    :param tgt: a PIL Image of the target image
+    :param crop_size: an HxW scale of the desired crop e.g. 480x852
+    :return: the cropped img & tgt tensors
+    """
+    t = transforms.RandomResizedCrop(crop_size)
+    state = torch.get_rng_state()
+    img = t(img)
+    torch.set_rng_state(state)
+    tgt = t(tgt)
+    return img, tgt
 
 
 def split_dataset(dataset, split_size: float = 0.25):
@@ -206,27 +195,76 @@ if __name__ == '__main__':
     #     print(i_batch, sample_batched['image'].size(),
     #           sample_batched['mask'].size())
 
-    uavid_rgb_vals = {
+    # uavid_rgb_vals = {
+    #     tuple([0, 0, 0]): 0,  # Background Clutter
+    #     tuple([128, 0, 0]): 1,  # Building
+    #     tuple([128, 64, 128]): 2,  # Road
+    #     tuple([0, 128, 0]): 3,  # Tree
+    #     tuple([128, 128, 0]): 4,  # Low Vegetation
+    #     tuple([64, 0, 128]): 5,  # Moving Car
+    #     tuple([192, 0, 192]): 6,  # Static Car
+    #     tuple([192, 0, 192]): 7  # Human
+    # }
+    #
+    # dir_name = os.path.dirname(__file__)  # get the location of the root directory
+    # dataset = "uavid"
+    # dir_name = os.path.join(dir_name, '../..')
+    # dir_name = os.path.join(dir_name, 'data/segmentation-datasets')
+    # dir_name = os.path.join(dir_name, dataset)
+    # uav_train_set = UAVDataset(os.path.join(dir_name, 'train'), uavid_rgb_vals)
+    # uav_test_set = UAVDataset(os.path.join(dir_name, 'test'), uavid_rgb_vals)
+    # uav_train_dataloader = DataLoader(uav_train_set, batch_size=4, shuffle=False, num_workers=20)
+    # uav_test_dataloader = DataLoader(uav_test_set, batch_size=4, shuffle=False, num_workers=20)
+    # for i_batch, sample_batched in enumerate(uav_test_dataloader):
+    #     print(i_batch, sample_batched['image'].size(),
+    #           sample_batched['mask'].size())
+    #     # img = sample_batched['image'][0].cpu().numpy()
+    #     # # rearrage order to HWC
+    #     # img = np.clip(img, 0, 1)
+    #     # plt.imshow(img)
+    #     # plt.show()
+
+    cityscapes_rgb_vals = {
         tuple([0, 0, 0]): 0,  # Background Clutter
-        tuple([128, 0, 0]): 1,  # Building
-        tuple([128, 64, 128]): 2,  # Road
-        tuple([0, 128, 0]): 3,  # Tree
-        tuple([128, 128, 0]): 4,  # Low Vegetation
-        tuple([64, 0, 128]): 5,  # Moving Car
-        tuple([192, 0, 192]): 6,  # Static Car
-        tuple([192, 0, 192]): 7  # Human
+        tuple([111, 74, 0]): 1,  # Dynamic Object
+        tuple([81, 0, 81]): 2,  # Ground
+        tuple([128, 64, 128]): 3,  # Road
+        tuple([244, 35, 232]): 4,  # Sidewalk
+        tuple([250, 170, 160]): 5,  # Parking
+        tuple([230, 150, 140]): 6,  # Rail-Track
+        tuple([70, 70, 70]): 7,  # building
+        tuple([102, 102, 156]): 8,  # wall
+        tuple([190, 153, 153]): 9,  # fence
+        tuple([180, 165, 180]): 10,  # guard-rail
+        tuple([150, 100, 100]): 11,  # bridge
+        tuple([150, 120, 90]): 12,  # tunnel
+        tuple([153, 153, 153]): 13,  # pole
+        tuple([250, 170, 30]): 14,  # traffic light
+        tuple([220, 220, 0]): 15,  # traffic sign
+        tuple([107, 142, 35]): 16,  # vegetation
+        tuple([152, 251, 152]): 17,  # terrain
+        tuple([70, 130, 180]): 18,  # sky
+        tuple([220, 20, 60]): 19,  # person
+        tuple([255, 0, 0]): 20,  # bike-rider
+        tuple([0, 0, 142]): 21,  # car
+        tuple([0, 0, 70]): 22,  # truck
+        tuple([0, 60, 100]): 23,  # bus
+        tuple([0, 0, 90]): 24,  # caravan
+        tuple([0, 0, 110]): 25,  # trailer
+        tuple([0, 80, 100]): 26,  # train
+        tuple([0, 0, 230]): 27,  # motorcycle
+        tuple([119, 11, 32]): 28,  # bicycle
+        tuple([0, 0, 142]): 30  # license plate
     }
 
     dir_name = os.path.dirname(__file__)  # get the location of the root directory
-    dataset = "uavid"
+    dataset = "cityscapes"
     dir_name = os.path.join(dir_name, '../..')
     dir_name = os.path.join(dir_name, 'data/segmentation-datasets')
     dir_name = os.path.join(dir_name, dataset)
-    uav_train_set = UAVDataset(os.path.join(dir_name, 'train'), uavid_rgb_vals)
-    uav_test_set = UAVDataset(os.path.join(dir_name, 'test'), uavid_rgb_vals)
-    uav_train_dataloader = DataLoader(uav_train_set, batch_size=4, shuffle=False, num_workers=20)
-    uav_test_dataloader = DataLoader(uav_test_set, batch_size=4, shuffle=False, num_workers=20)
-    for i_batch, sample_batched in enumerate(uav_test_dataloader):
+    city_set = CityscapesDataset(dir_name, cityscapes_rgb_vals)
+    city_set_dataloader = DataLoader(city_set, batch_size=4, shuffle=False, num_workers=20)
+    for i_batch, sample_batched in enumerate(city_set_dataloader):
         print(i_batch, sample_batched['image'].size(),
               sample_batched['mask'].size())
         # img = sample_batched['image'][0].cpu().numpy()
@@ -234,8 +272,7 @@ if __name__ == '__main__':
         # img = np.clip(img, 0, 1)
         # plt.imshow(img)
         # plt.show()
-
-
+    print('fin')
 
 # for idx in range(len(test_set)):
 #     sample = test_set[idx]
