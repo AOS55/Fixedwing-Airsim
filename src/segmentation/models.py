6da656ceb071f654fe90df81fb432a5cff8a7292
Alexander Quessy <aq15777@bristol.ac.uk>
2021-04-29 14:05:10 +0100

    changed model setup

--- src/segmentation/models.py
+++ src/segmentation/models.py
@@ -1,6 +1,6 @@
 # import segmentation_models_pytorch as smp
 import torch
-# import torchvision.models as models
+import torchvision.models.segmentation as models
 
 
 def get_model(model_name: str, device, num_classes: int, pretrained: bool = False) -> torch.nn:
@@ -13,20 +13,19 @@ def get_model(model_name: str, device, num_classes: int, pretrained: bool = Fals
     :param pretrained: boolean of starting on a pretrained model
     :return: the nn.model to be trained
     """
-    model_dict = {'fcn_resnet50': torch.hub.load('pytorch/vision', 'fcn_resnet50', pretrained=pretrained,
-                                                 num_classes=num_classes).to(device).eval(),
-                  'fcn_resnet101': torch.hub.load('pytorch/vision', 'fcn_resnet101', pretrained=pretrained,
-                                                  num_classes=num_classes).to(device).eval(),
-                  'deeplabv3_resnet50': torch.hub.load('pytorch/vision', 'deeplabv3_resnet50', pretrained=pretrained,
-                                                       num_classes=num_classes).to(device).eval(),
-                  'deeplabv3_resnet101': torch.hub.load('pytorch/vision', 'deeplabv3_resnet101',
-                                                        pretrained=pretrained, num_classes=num_classes).to(
+    print(pretrained)
+    model_dict = {'fcn_resnet50': models.fcn_resnet50(pretrained=pretrained, num_classes=num_classes).to(device).eval(),
+                  'fcn_resnet101': models.fcn_resnet101(pretrained=pretrained,
+                                                        num_classes=num_classes).to(device).eval(),
+                  'deeplabv3_resnet50': models.deeplabv3_resnet50(pretrained=pretrained,
+                                                                  num_classes=num_classes).to(device).eval(),
+                  'deeplabv3_resnet101': models.deeplabv3_resnet101(pretrained=pretrained,
+                                                                    num_classes=num_classes).to(device).eval(),
+                  'deeplabv3_mobilenet_v3_large': models.deeplabv3_mobilenet_v3_large(pretrained=pretrained,
+                                                                                      num_classes=num_classes).to(
                       device).eval(),
-                  'deeplabv3_mobilenet_v3_large': torch.hub.load('pytorch/vision', 'deeplabv3_mobilenet_v3_large',
-                                                                 pretrained=pretrained, num_classes=num_classes).to(
-                      device).eval(),
-                  'lraspp_mobilenet_v3_large': torch.hub.load('pytorch/vision', 'lraspp_mobilenet_v3_large',
-                                                              pretrained=pretrained, num_classes=num_classes).to(
+                  'lraspp_mobilenet_v3_large': models.lraspp_mobilenet_v3_large(pretrained=pretrained,
+                                                                                num_classes=num_classes).to(
                       device).eval()
                   }
     try:
@@ -34,6 +33,7 @@ def get_model(model_name: str, device, num_classes: int, pretrained: bool = Fals
     except KeyError:
         print(f"KeyError, model_name is not valid allowable names are: {model_dict.keys()}")
         model = None
+    model = model.eval()
     return model
 
 
